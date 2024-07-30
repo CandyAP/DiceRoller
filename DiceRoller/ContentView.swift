ec1daf12bd84219b22f53ef2c0ff5de6116fb229
candyap <candyap182@icloud.com>
2024-07-30 18:03:07 +0700

    add "remove button" & "add button"

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
     var body: some View {
         VStack {
-            Text("Aliens Do Fucking Exist")
+            Text("Dice Roller")
+                .font(.largeTitle)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) {
+                    _ in DiceView()
+                }
+                
+                
+            }
+            
+            HStack {
+                Button("Remove Dice") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 3)
+            }
+            .padding()
         }
         .padding()
     }
