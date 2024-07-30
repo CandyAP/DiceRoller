7771c37ced2905f988cf3d1b4c6ab8190adaabad
candyap <candyap182@icloud.com>
2024-07-30 17:30:55 +0700

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
